19dcb869c3163dbded669be029fdb1369eb264d8
Jose Villa <j0542420@amarillocollege.com>
2025-01-25 18:07:28 -0600

    update

--- Program.cs
+++ Program.cs
@@ -10,6 +10,29 @@ namespace Wk2Ex1
     {
         static void Main(string[] args)
         {
+            //declarations
+            int principal = '0';
+            double rate = '0';
+            int time = '0';
+            int interest = (int)(principal * rate * time);
+            //input principal of the loan
+            Console.WriteLine("What is principal amount of the loan in dollars?");
+            //collect the user input for the principal of the loan
+            principal = Convert.ToInt32(Console.ReadLine());
+
+            //input the interest rate
+            Console.WriteLine("What is the interest rate(input 0.05 for 5%)?");
+            //collect the user input for the interest rate
+            rate = Convert.ToInt32(Console.ReadLine());
+
+            //input the time in years to pay of the loan
+            Console.WriteLine("What is the period of loan in years?");
+            //collect the user input for the time to pay of the loan
+            time = Convert.ToInt32(Console.ReadLine());
+
+            //calculate the interest of the loan
+            Console.WriteLine($"Total interest of loan: {interest} dollars");
+            
         }
     }
 }
